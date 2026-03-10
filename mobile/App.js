6f70d0dd0c987b5f736e114140ebd46fa165ff70
B V Nagireddy <70723307+Niteesh57@users.noreply.github.com>
2026-03-10 20:19:37 +0530

    AI Initialized: mobile/App.js

--- mobile/App.js
+++ mobile/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import QRScannerScreen from './screens/QRScannerScreen';
+import RouteOptimizationScreen from './screens/RouteOptimizationScreen';
 
 const Stack = createStackNavigator();
 
@@ -9,7 +10,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="QRScanner">
-        <Stack.Screen name="QRScanner" component={QRScannerScreen} options={{ title: 'Scan Shipment QR' }} />
+        <Stack.Screen name="QRScanner" component={QRScannerScreen} />
+        <Stack.Screen name="RouteOptimization" component={RouteOptimizationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
